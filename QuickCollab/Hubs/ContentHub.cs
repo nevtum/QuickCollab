9602d22f200e239a33aed35c4ac2896ace83953a
Neville Tummon <nt.devs@gmail.com>
2014-06-29 23:22:56 +1000

    added skeleton, removing connection from session

--- QuickCollab/Hubs/ContentHub.cs
+++ QuickCollab/Hubs/ContentHub.cs
@@ -3,14 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Microsoft.AspNet.SignalR;
+using System.Threading.Tasks;
+using QuickCollab.Session;
 
 namespace QuickCollab.Hubs
 {
     public class ContentHub : Hub
     {
+        private ISessionInstanceRepository _sessionManager;
+
+        public ContentHub()
+        {
+            _sessionManager = new SessionInstanceRepository();
+        }
+
         public void BroadcastMessage(string message)
         {
             Clients.All.NotifyBroadcasted(message);
+        }
+
+        public override Task OnDisconnected()
+        {
+            // Find connection and Remove from session
+
+            return base.OnDisconnected();
         }
     }
 }
