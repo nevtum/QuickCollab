ae8e317d2acf07162a00b754f6659290e593db4a
Neville Tummon <nt.devs@gmail.com>
2014-07-02 21:33:48 +1000

    cleaned up methods and dependencies

--- QuickCollab.Session/RegistrationService.cs
+++ QuickCollab.Session/RegistrationService.cs
@@ -10,15 +10,58 @@ namespace QuickCollab.Session
     public class RegistrationService
     {
         private ISessionInstanceRepository _repo;
+        private PasswordHashService _passwordService;
 
         public RegistrationService()
         {
             _repo = new SessionInstanceRepository();
+            _passwordService = new PasswordHashService();
         }
 
         public void RegisterConnection(string clientName, string sessionName)
         {
+            if (UserRegisteredWithSession(clientName, sessionName))
+                return;
+
             _repo.RegisterConnection(clientName, sessionName);
+        }
+
+        public void UnRegisterConnection(string clientName, string sessionName)
+        {
+            if (!UserRegisteredWithSession(clientName, sessionName))
+                return;
+
+            Connection conn = _repo
+                .GetConnectionsByUserName(clientName)
+                .Single(c => c.SessionName == sessionName);
+
+            _repo.UnRegisterConnection(conn);
+        }
+
+        public IEnumerable<string> CurrentSessions(string clientName)
+        {
+            return _repo.GetConnectionsByUserName(clientName)
+                .Select(c => c.SessionName);
+        }
+
+        public bool ValidatePassword(string sessionName, string password)
+        {
+            SessionInstance s = _repo.GetSession(sessionName);
+
+            // No password required
+            if (string.IsNullOrEmpty(s.HashedPassword))
+                return true;
+
+            if (_passwordService.SaltedPassword(password, s.Salt) == s.HashedPassword)
+                return true;
+
+            return false;
+        }
+
+        public bool UserRegisteredWithSession(string userName, string sessionName)
+        {
+            return _repo.GetConnectionsInSession(sessionName)
+                .Any(conn => conn.ClientName == userName);
         }
 
         public void StartNewSession(string sessionName, bool isVisible, string password)
@@ -35,9 +78,8 @@ namespace QuickCollab.Session
 
             if (!string.IsNullOrEmpty(password))
             {
-                PasswordHashService hasher = new PasswordHashService();
-                string salt = hasher.GetNewSalt();
-                string hashedPassword = hasher.SaltedPassword(password, salt);
+                string salt = _passwordService.GetNewSalt();
+                string hashedPassword = _passwordService.SaltedPassword(password, salt);
 
                 instance.Salt = salt;
                 instance.HashedPassword = hashedPassword;
--- QuickCollab/Hubs/ContentHub.cs
+++ QuickCollab/Hubs/ContentHub.cs
@@ -10,45 +10,75 @@ namespace QuickCollab.Hubs
 {
     public class ContentHub : Hub
     {
-        private ISessionInstanceRepository _repo;
+        private RegistrationService _registrationService;
 
         public ContentHub()
         {
-            _repo = new SessionInstanceRepository();
+            _registrationService = new RegistrationService();
         }
 
-        public void BroadcastMessage(string message)
+        public Task BroadcastMessage(string message)
         {
-            Clients.All.RecieveBroadcast(message);
+            string content = string.Format("[{0}]: {1}", DateTime.Now, message);
+
+            return Clients.All.RecieveBroadcast(content);
         }
 
-        [Authorize]
-        public void SendMessage(string sessionName, string message)
+        public Task SendMessage(string sessionName, string message)
         {
-            if (!Context.User.Identity.IsAuthenticated)
-                return;
+            return new Task(() =>
+            {
+                if (!_registrationService.UserRegisteredWithSession(Context.ConnectionId, sessionName))
+                    return;
 
-            if (!UserRegisteredWithSession(Context.User.Identity.Name, sessionName))
-                return;
-            
-            Clients.Group(sessionName).RecieveMessage(sessionName, message);
+                Clients.Group(sessionName).RecieveMessage(sessionName, message);
+            });
+        }
+
+        public Task JoinSession(string sessionName, string password = "")
+        {
+            return new Task(() =>
+            {
+                if (!Context.User.Identity.IsAuthenticated)
+                    return;
+
+                if (!_registrationService.ValidatePassword(sessionName, password))
+                    return;
+
+                _registrationService.RegisterConnection(Context.ConnectionId, sessionName);
+
+                Groups.Add(Context.ConnectionId, sessionName);
+
+                Clients.Group(sessionName)
+                    .RecieveMessage(sessionName, string.Format("{0} has joined session.", Context.User.Identity));
+            });
+        }
+
+        public Task LeaveSession(string sessionName)
+        {
+            return new Task(() =>
+            {
+                if (!_registrationService.UserRegisteredWithSession(Context.ConnectionId, sessionName))
+                    return;
+
+                _registrationService.UnRegisterConnection(Context.ConnectionId, sessionName);
+
+                Groups.Remove(Context.ConnectionId, sessionName);
+
+                Clients.Group(sessionName)
+                    .RecieveMessage(sessionName, string.Format("{0} has left session.", Context.User.Identity));
+            });
         }
 
         public override Task OnDisconnected()
         {
-            foreach (Connection conn in _repo.GetConnectionsByUserName(Context.User.Identity.Name))
+            return new Task(() =>
             {
-                _repo.UnRegisterConnection(conn);
-            }
-
-            return base.OnDisconnected();
-        }
-
-        // perhaps an extension method?
-        private bool UserRegisteredWithSession(string userName, string sessionName)
-        {
-            return _repo.GetConnectionsInSession(sessionName)
-                .Any(conn => conn.ClientName == userName);
+                foreach (string sessionName in _registrationService.CurrentSessions(Context.ConnectionId))
+                {
+                    LeaveSession(sessionName);
+                }
+            });
         }
     }
 }
