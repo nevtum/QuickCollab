5745639df6d61b764bb1296f0e3458d98436fa64
Neville Tummon <nt.devs@gmail.com>
2014-07-24 21:30:11 +1000

    binding directly to rest api

--- QuickCollab/Scripts/App/ChatApp.js
+++ QuickCollab/Scripts/App/ChatApp.js
@@ -1,36 +1,7 @@
-﻿var chatApp = angular.module('chatApp', ['ngRoute']);
+﻿var chatApp = angular.module('chatApp', ['ngRoute', 'Lobby.Controllers']);
 
 chatApp.config(function ($routeProvider) {
     $routeProvider
         .when('/', { controller: 'SessionListCtrl', templateUrl: '../Templates/SessionList.html' })
         .otherwise({ redirectTo: '/' });
 });
-
-chatApp.controller('SessionListCtrl', ['$scope', '$http', function ($scope, $http, $location) {
-    $scope.sessions = [
-        { DateCreated: "12/12/12", SessionName: "The coolies", PersistHistory: true, Secured: false },
-        { DateCreated: "12/12/12", SessionName: "Wassup!", PersistHistory: true, Secured: false },
-        { DateCreated: "12/12/12", SessionName: "--== Naruto Fans ==--", PersistHistory: true, Secured: false },
-        { DateCreated: "12/12/12", SessionName: "My mockup Session ABC", PersistHistory: true, Secured: false },
-        { DateCreated: "12/12/12", SessionName: "Fort Knox", PersistHistory: true, Secured: true, Uri: '/Fort%Knox'},
-        { DateCreated: "12/12/12", SessionName: "My mockup Session ABC", PersistHistory: false, Secured: false },
-        { DateCreated: "12/12/12", SessionName: "Testing testing", PersistHistory: true, Secured: true, Uri: '/Testing%Testing' },
-        { DateCreated: "12/12/12", SessionName: "Hey there!", PersistHistory: true, Secured: false },
-    ];
-
-    $scope.authenticate = function (session) {
-        // show modal view to fill in password
-        // post registration on click of ok button
-        // if failed display error message
-        // if passed display verified icon then close modal dialog
-        console.log("open authentication dialog for session " + session.Uri);
-        console.log(session);
-    };
-
-    $scope.open = function (session) {
-        // add session to current tabs open
-        // subscribe to signalr chat room
-        // make sure to not allow to open another tab if same session open already
-        console.log("Joining " + session.SessionName);
-    };
-}]);
---
+++ QuickCollab/Scripts/App/Lobby/SessionListCtrl.js
@@ -0,0 +1,25 @@
+﻿var chatApp = angular.module('Lobby.Controllers', []);
+
+chatApp.controller('SessionListCtrl', ['$scope', '$http', function ($scope, $http, $location) {
+
+    $scope.authenticate = function (session) {
+        // show modal view to fill in password
+        // post registration on click of ok button
+        // if failed display error message
+        // if passed display verified icon then close modal dialog
+        console.log("open authentication dialog for session " + session.Uri);
+        console.log(session);
+    };
+
+    $scope.open = function (session) {
+        // add session to current tabs open
+        // subscribe to signalr chat room
+        // make sure to not allow to open another tab if same session open already
+        console.log("Joining " + session.SessionName);
+    };
+
+    $http.get('../api/SessionList').success(function (data) {
+        $scope.sessions = data;
+    });
+
+}]);
